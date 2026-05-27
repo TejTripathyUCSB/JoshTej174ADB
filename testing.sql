3617b306767073f4c77cb61b835af6f5279fa257
Joshua Alexander <joshua.alexander0527@gmail.com>
2026-05-27 14:17:52 -0700

    gui

--- testing.sql
+++ testing.sql
@@ -1,1 +1,1 @@
-DELETE FROM edepot_shipping_notice_items WHERE notice_id='N100'; DELETE FROM edepot_shipping_notices WHERE notice_id='N100'; UPDATE item SET replenishment = replenishment - 1 WHERE stock_number='AA00101'; COMMIT;
+DROP TABLE instructors CASCADE CONSTRAINTS;
