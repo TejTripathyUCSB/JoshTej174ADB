59ceb44485dc30920b3a938a320bf793d753fe74
Joshua Alexander <joshua.alexander0527@gmail.com>
2026-05-27 19:02:48 -0700

    changes and stuff

--- testing.sql
+++ testing.sql
@@ -1,1 +1,3 @@
-DROP TABLE instructors CASCADE CONSTRAINTS;
+SELECT * FROM edepot_shipping_notices WHERE notice_id = 'N_BAD';     -- 0 rows
+SELECT * FROM edepot_shipping_notice_items WHERE notice_id = 'N_BAD'; -- 0 rows
+SELECT replenishment FROM item WHERE stock_number = 'AA00201';       -- unchanged
